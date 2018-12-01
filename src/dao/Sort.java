2f48b3ba3530e42e0a94fab575c26115d9dcbf5a
Justin Cheng <Yusi.Cheng.0509@gmail.com>
2018-11-30 17:35:20 -0800

    test comment out

--- src/dao/Sort.java
+++ src/dao/Sort.java
@@ -7,16 +7,16 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Sort {
-    class SortbyBear implements Comparator {
+    class SortbyYear implements Comparator {
         @Override
         public int compare(Object o1, Object o2) {
             Vehicle v1 = (Vehicle) o1;
             Vehicle v2 = (Vehicle) o2;
-            if (Integer.valueOf(v1.getYear()).compareTo(Integer.valueOf(v2.getYear())) == 0) {
+            if (Integer.valueOf(v1.getYear()).equals(Integer.valueOf(v2.getYear()))) {
                 return 0;
-            } else if (Integer.valueOf(v1.getYear()).compareTo(Integer.valueOf(v2.getYear())) > 0) {
+            } else if (Integer.valueOf(v1.getYear())>(Integer.valueOf(v2.getYear()))) {
                 return 1;
-            } else if (Integer.valueOf(v1.getYear()).compareTo(Integer.valueOf(v2.getYear())) < 0) {
+            } else if (Integer.valueOf(v1.getYear())<(Integer.valueOf(v2.getYear()))) {
                 return -1;
             } else {
                 return 0;
@@ -32,11 +32,11 @@ public class Sort {
         public int compare(Object o1, Object o2) {
             Vehicle v1 = (Vehicle) o1;
             Vehicle v2 = (Vehicle) o2;
-            if (Integer.valueOf(v1.getPrice()).compareTo(Integer.valueOf(v2.getPrice())) == 0) {
+            if (Integer.valueOf(v1.getPrice()).equals(Integer.valueOf(v2.getPrice()))) {
                 return 0;
-            } else if (Integer.valueOf(v1.getPrice()).compareTo(Integer.valueOf(v2.getPrice())) > 0) {
+            } else if (Integer.valueOf(v1.getPrice())>(Integer.valueOf(v2.getPrice()))) {
                 return 1;
-            } else if (Integer.valueOf(v1.getPrice()).compareTo(Integer.valueOf(v2.getPrice())) < 0) {
+            } else if (Integer.valueOf(v1.getPrice())<(Integer.valueOf(v2.getPrice()))) {
                 return -1;
             } else {
                 return 0;
@@ -44,7 +44,7 @@ public class Sort {
         }
     }
 
-    public static void main(String args[]) {
+  /*  public static void main(String args[]) {
         Vehicle v1 = new Vehicle("a", "b", "1993", "d",
                 "e", "f", "g", "1800", "i", "j");
         Vehicle v2 = new Vehicle("a", "b", "1994", "d",
@@ -55,7 +55,7 @@ public class Sort {
         inventory.add(v1);
         inventory.add(v2);
         inventory.add(v3);
-        Collections.sort(inventory, new sortbyPrice());
-    }
+        Collections.sort(inventory, new SortByPrice());
+    }*/
 }
 
