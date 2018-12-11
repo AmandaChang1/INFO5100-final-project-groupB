396487e73846bc9c687aa3df3141d676f555f24e
SayaliGirish <SayaliGirish@10.15.70.247>
2018-12-11 14:41:32 -0800

    Car Search Ui Update

--- src/ui/CarSearch.java
+++ src/ui/CarSearch.java
@@ -130,6 +130,10 @@ public class CarSearch extends CarSearchDefination implements ActionListener {
 		 brandFilter.setSelectedIndex(brandFilter.getItemCount()-1);
 		 modelFilter.setSelectedIndex(modelFilter.getItemCount()-1);
 		 bodyTypeFilter.setSelectedIndex(bodyTypeFilter.getItemCount()-1);
+		 maxYearFilter.setSelectedIndex(maxYearFilter.getItemCount()-1);
+		 minYearFilter.setSelectedIndex(1);
+		 maxPriceFilter.setSelectedIndex(maxPriceFilter.getItemCount()-1);
+		 minPriceFilter.setSelectedIndex(1);
 		 categoryFilter1.setSelected(false);
 		 categoryFilter2.setSelected(true);
 		 categoryFilter3.setSelected(false);
