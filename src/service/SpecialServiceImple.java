24ebbd726c896a1dd1061ef1dadd9d526c0b9da7
Yaqi Wang <wangyaqi9506@gmail.com>
2018-12-12 19:42:00 -0800

    final version

---
+++ src/service/SpecialServiceImple.java
@@ -0,0 +1,45 @@
+package service;
+
+
+import dao.ManageSpecial;
+import dao.ManageSpecialImple;
+
+import dto.Special;
+import dto.Specials;
+
+public class SpecialServiceImple implements SpecialService {
+
+    private ManageSpecial manageSpecial;
+
+    public SpecialServiceImple() {
+        manageSpecial=new ManageSpecialImple();
+    }
+
+    @Override
+    public void addSpecial(Special special) {
+        manageSpecial.addSpecial(special);
+
+    }
+
+    @Override
+    public void deleteSpecial(Special special) {
+
+        manageSpecial.deleteSpecial(special);
+
+    }
+
+    @Override
+    public void updateSpecial(Special special) {
+
+        manageSpecial.updateSpecial(special);
+
+    }
+
+    @Override
+    public Specials getSpecialsByDealer(String dealerId) {
+
+        Specials specials=manageSpecial.getSpecialsByDealer(dealerId);
+        return specials;
+
+    }
+}
